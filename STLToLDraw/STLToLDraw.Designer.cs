074696bfbd1eaaea5f5e99733c1b667abac49b3a
kevin <sudo@pt-get.com>
2015-10-24 22:13:36 -0700

    init

---
+++ STLToLDraw/STLToLDraw.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace STLToLDraw
+{
+	partial class STLToLDraw
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._openButton = new System.Windows.Forms.Button();
+			this._fileLabel = new System.Windows.Forms.Label();
+			this._infoLabel = new System.Windows.Forms.Label();
+			this._convertButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// _openButton
+			// 
+			this._openButton.Location = new System.Drawing.Point(13, 13);
+			this._openButton.Name = "_openButton";
+			this._openButton.Size = new System.Drawing.Size(75, 23);
+			this._openButton.TabIndex = 0;
+			this._openButton.Text = "Open";
+			this._openButton.UseVisualStyleBackColor = true;
+			this._openButton.Click += new System.EventHandler(this.OpenButtonOnClick);
+			// 
+			// _fileLabel
+			// 
+			this._fileLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._fileLabel.Location = new System.Drawing.Point(12, 39);
+			this._fileLabel.Name = "_fileLabel";
+			this._fileLabel.Size = new System.Drawing.Size(437, 20);
+			this._fileLabel.TabIndex = 1;
+			// 
+			// _infoLabel
+			// 
+			this._infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this._infoLabel.Location = new System.Drawing.Point(12, 59);
+			this._infoLabel.Name = "_infoLabel";
+			this._infoLabel.Size = new System.Drawing.Size(437, 20);
+			this._infoLabel.TabIndex = 2;
+			// 
+			// _convertButton
+			// 
+			this._convertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this._convertButton.Location = new System.Drawing.Point(374, 12);
+			this._convertButton.Name = "_convertButton";
+			this._convertButton.Size = new System.Drawing.Size(75, 23);
+			this._convertButton.TabIndex = 3;
+			this._convertButton.Text = "Convert";
+			this._convertButton.UseVisualStyleBackColor = true;
+			this._convertButton.Click += new System.EventHandler(this.ConvertButtonOnClick);
+			// 
+			// STLToLDraw
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(461, 219);
+			this.Controls.Add(this._convertButton);
+			this.Controls.Add(this._infoLabel);
+			this.Controls.Add(this._fileLabel);
+			this.Controls.Add(this._openButton);
+			this.Name = "STLToLDraw";
+			this.Text = "STLToLDraw";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button _openButton;
+		private System.Windows.Forms.Label _fileLabel;
+		private System.Windows.Forms.Label _infoLabel;
+		private System.Windows.Forms.Button _convertButton;
+	}
+}
+
---
+++ STLToLDraw/STLToLDraw.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace STLToLDraw
+{
+	public partial class STLToLDraw : Form
+	{
+		private OpenFileDialog _ofd;
+
+		public STLToLDraw()
+		{
+			InitializeComponent();
+		}
+
+		private void OpenButtonOnClick(object sender, EventArgs e)
+		{
+			var _ofd = new OpenFileDialog { Filter = "Stereolithography Files|*.stl" };
+			if (_ofd.ShowDialog() == DialogResult.OK)
+			{
+				using (BinaryReader br = new BinaryReader(_ofd.OpenFile()))
+				{
+					byte[] header = br.ReadBytes(5);
+
+					if (Encoding.ASCII.GetString(header) == "solid")
+					{
+						_infoLabel.Text = "ASCII STL files not supported.";
+						_ofd = null;
+						return;
+					}
+					else
+					{
+						_infoLabel.Text = "";
+					}
+				}
+			}
+			else
+			{
+				_ofd = null;
+			}
+		}
+
+		private void ConvertButtonOnClick(object sender, EventArgs e)
+		{
+			if (_ofd != null)
+			{
+				using (BinaryReader br = new BinaryReader(_ofd.OpenFile()))
+				{
+					byte[] header = br.ReadBytes(80);
+					uint n = br.ReadUInt32();
+
+					for (int i = 0; i < n; i++)
+					{
+						// normal
+						br.ReadSingle();
+						br.ReadSingle();
+						br.ReadSingle();
+
+						Vector3 a = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+						Vector3 b = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+						Vector3 c = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+						Triangle t = new Triangle(a, b, c);
+					}
+				}
+			}
+		}
+
+		private struct Vector3
+		{
+			public double X;
+			public double Y;
+			public double Z;
+
+			public Vector3(double x, double y, double z)
+			{
+				X = x;
+				Y = y;
+				Z = z;
+			}
+
+			public double Length
+			{
+				get { return Math.Sqrt(X * X + Y * Y + Z * Z); }
+			}
+
+			public Vector3 Norm
+			{
+				get { double len = Length; return new Vector3(X / len, Y / len, Z / len); }
+			}
+
+
+			public double Dot(Vector3 other)
+			{
+				return X * other.X + Y * other.Y + Z * other.Z;
+			}
+
+			public Vector3 Cross(Vector3 other)
+			{
+				return new Vector3(Y * other.Z - Z * other.Y, Z * other.X - X * other.Z, X * other.Y - Y * other.Z);
+			}
+
+			public static Vector3 operator -(Vector3 a, Vector3 b)
+			{
+				return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+			}
+
+			public static Vector3 operator +(Vector3 a, Vector3 b)
+			{
+				return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+			}
+		}
+
+		private struct Triangle
+		{
+			public Vector3 A;
+			public Vector3 B;
+			public Vector3 C;
+
+			public Triangle(Vector3 a, Vector3 b, Vector3 c)
+			{
+				A = a;
+				B = b;
+				C = c;
+			}
+
+			public Vector3 Normal
+			{
+				get { return (B - A).Cross(C - A).Norm; }
+			}
+		}
+	}
+}
