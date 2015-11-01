d654907c96b2c1ba7f712f8dea86d9d46802cbf0
kevin <sudo@pt-get.com>
2015-10-31 20:00:21 -0700

    finalize

--- STLToLDraw/STLToLDraw.Designer.cs
+++ STLToLDraw/STLToLDraw.Designer.cs
@@ -29,14 +29,17 @@
 		private void InitializeComponent()
 		{
 			this._openButton = new System.Windows.Forms.Button();
-			this._fileLabel = new System.Windows.Forms.Label();
 			this._infoLabel = new System.Windows.Forms.Label();
 			this._convertButton = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this._scaleBox = new System.Windows.Forms.NumericUpDown();
+			this._progressBar = new System.Windows.Forms.ProgressBar();
+			((System.ComponentModel.ISupportInitialize)(this._scaleBox)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// _openButton
 			// 
-			this._openButton.Location = new System.Drawing.Point(13, 13);
+			this._openButton.Location = new System.Drawing.Point(12, 12);
 			this._openButton.Name = "_openButton";
 			this._openButton.Size = new System.Drawing.Size(75, 23);
 			this._openButton.TabIndex = 0;
@@ -44,20 +47,11 @@
 			this._openButton.UseVisualStyleBackColor = true;
 			this._openButton.Click += new System.EventHandler(this.OpenButtonOnClick);
 			// 
-			// _fileLabel
-			// 
-			this._fileLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this._fileLabel.Location = new System.Drawing.Point(12, 39);
-			this._fileLabel.Name = "_fileLabel";
-			this._fileLabel.Size = new System.Drawing.Size(437, 20);
-			this._fileLabel.TabIndex = 1;
-			// 
 			// _infoLabel
 			// 
 			this._infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this._infoLabel.Location = new System.Drawing.Point(12, 59);
+			this._infoLabel.Location = new System.Drawing.Point(12, 36);
 			this._infoLabel.Name = "_infoLabel";
 			this._infoLabel.Size = new System.Drawing.Size(437, 20);
 			this._infoLabel.TabIndex = 2;
@@ -73,27 +67,67 @@
 			this._convertButton.UseVisualStyleBackColor = true;
 			this._convertButton.Click += new System.EventHandler(this.ConvertButtonOnClick);
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(95, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(194, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Scale Factor (STL units to LDraw Units)";
+			// 
+			// _scaleBox
+			// 
+			this._scaleBox.DecimalPlaces = 3;
+			this._scaleBox.Location = new System.Drawing.Point(295, 13);
+			this._scaleBox.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+			this._scaleBox.Name = "_scaleBox";
+			this._scaleBox.Size = new System.Drawing.Size(73, 20);
+			this._scaleBox.TabIndex = 5;
+			this._scaleBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// _progressBar
+			// 
+			this._progressBar.Location = new System.Drawing.Point(15, 60);
+			this._progressBar.Name = "_progressBar";
+			this._progressBar.Size = new System.Drawing.Size(434, 23);
+			this._progressBar.TabIndex = 6;
+			// 
 			// STLToLDraw
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(461, 219);
+			this.Controls.Add(this._progressBar);
+			this.Controls.Add(this._scaleBox);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this._convertButton);
 			this.Controls.Add(this._infoLabel);
-			this.Controls.Add(this._fileLabel);
 			this.Controls.Add(this._openButton);
 			this.Name = "STLToLDraw";
 			this.Text = "STLToLDraw";
+			((System.ComponentModel.ISupportInitialize)(this._scaleBox)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button _openButton;
-		private System.Windows.Forms.Label _fileLabel;
 		private System.Windows.Forms.Label _infoLabel;
 		private System.Windows.Forms.Button _convertButton;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown _scaleBox;
+		private System.Windows.Forms.ProgressBar _progressBar;
 	}
 }
 
--- STLToLDraw/STLToLDraw.cs
+++ STLToLDraw/STLToLDraw.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace STLToLDraw
@@ -16,9 +18,10 @@ namespace STLToLDraw
 
 		private void OpenButtonOnClick(object sender, EventArgs e)
 		{
-			var _ofd = new OpenFileDialog { Filter = "Stereolithography Files|*.stl" };
+			_ofd = new OpenFileDialog { Filter = "Stereolithography Files|*.stl" };
 			if (_ofd.ShowDialog() == DialogResult.OK)
 			{
+				/*
 				using (BinaryReader br = new BinaryReader(_ofd.OpenFile()))
 				{
 					byte[] header = br.ReadBytes(5);
@@ -34,36 +37,136 @@ namespace STLToLDraw
 						_infoLabel.Text = "";
 					}
 				}
+				*/
+				_convertButton.Enabled = true;
+				_infoLabel.Text = _ofd.FileName;
 			}
 			else
 			{
 				_ofd = null;
+				_convertButton.Enabled = false;
 			}
 		}
 
-		private void ConvertButtonOnClick(object sender, EventArgs e)
+		private async void ConvertButtonOnClick(object sender, EventArgs e)
 		{
+			_convertButton.Enabled = false;
 			if (_ofd != null)
 			{
-				using (BinaryReader br = new BinaryReader(_ofd.OpenFile()))
+				SaveFileDialog sfd = new SaveFileDialog { Filter = "LDraw Data File|*.dat", FileName = "Part.dat" };
+				if (sfd.ShowDialog() == DialogResult.OK)
 				{
-					byte[] header = br.ReadBytes(80);
-					uint n = br.ReadUInt32();
-
-					for (int i = 0; i < n; i++)
+					await Task.Run(new Action(() =>
 					{
-						// normal
-						br.ReadSingle();
-						br.ReadSingle();
-						br.ReadSingle();
+						Invoke((MethodInvoker) delegate { _progressBar.Value = 0; });
 
-						Vector3 a = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
-						Vector3 b = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
-						Vector3 c = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
-						Triangle t = new Triangle(a, b, c);
-					}
+						using (Stream output = sfd.OpenFile())
+						using (BinaryReader br = new BinaryReader(_ofd.OpenFile()))
+						{
+							byte[] header = br.ReadBytes(80);
+							uint n = br.ReadUInt32();
+
+							Invoke((MethodInvoker) delegate { _progressBar.Maximum = (int) n; });
+
+							HashSet<Tuple<Vector3, Vector3, Triangle>> edges = new HashSet<Tuple<Vector3, Vector3, Triangle>>();
+
+							for (int i = 0; i < n; i++)
+							{
+								if (i % 500 == 0)
+									Invoke((MethodInvoker) delegate { _progressBar.Value = i; }); 
+
+								// normal
+								Vector3 normal = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+
+								// triangle
+								Vector3 a = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle()) * (double) _scaleBox.Value;
+								Vector3 b = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle()) * (double) _scaleBox.Value;
+								Vector3 c = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle()) * (double) _scaleBox.Value;
+
+								// attrs
+								br.ReadUInt16();
+								Triangle t = new Triangle(a, b, c, normal);
+
+								// assume colour 16
+								string tri = string.Format("3 16 {0:F} {1:F} {2:F} {3:F} {4:F} {5:F} {6:F} {7:F} {8:F}\n",
+									t.A.X, t.A.Y, t.A.Z,
+									t.B.X, t.B.Y, t.B.Z,
+									t.C.X, t.C.Y, t.C.Z);
+								output.Write(Encoding.ASCII.GetBytes(tri), 0, tri.Length);
+
+								var curedges = t.Edges;
+								foreach (var edge in curedges)
+								{
+									// use oct-tree for efficiency... later
+									Tuple<Vector3, Vector3, Triangle> match = null;
+									foreach (var j in edges)
+									{
+										const double epsilon = 0.000001;
+										if ((j.Item1 - edge.Item1).LenSq < epsilon && (j.Item2 - edge.Item2).LenSq < epsilon)
+										{
+											match = j;
+											break;
+										}
+										else if ((j.Item1 - edge.Item2).LenSq < epsilon && (j.Item2 - edge.Item1).LenSq < epsilon)
+										{
+											match = j;
+											break;
+										}
+									}
+
+									if (match != null)
+									{
+										double dot = match.Item3.Normal.Dot(edge.Item3.Normal);
+
+										if (dot < 0.9999)
+										{
+											if (dot > 0.20)
+											{
+												// draw optional line
+												Vector3 c1 = edge.Item3.Centre, c2 = match.Item3.Centre;
+												string line = string.Format("5 24 {0:F} {1:F} {2:F} {3:F} {4:F} {5:F} {6:F} {7:F} {8:F} {9:F} {10:F} {11:F}\n",
+												edge.Item1.X, edge.Item1.Y, edge.Item1.Z,
+												edge.Item2.X, edge.Item2.Y, edge.Item2.Z,
+												c1.X, c1.Y, c1.Z,
+												c2.X, c2.Y, c2.Z);
+												output.Write(Encoding.ASCII.GetBytes(line), 0, line.Length);
+											}
+											else
+											{
+												// permanent line
+												string line = string.Format("2 24 {0:F} {1:F} {2:F} {3:F} {4:F} {5:F}\n",
+													edge.Item1.X, edge.Item1.Y, edge.Item1.Z,
+													edge.Item2.X, edge.Item2.Y, edge.Item2.Z);
+												output.Write(Encoding.ASCII.GetBytes(line), 0, line.Length);
+											}
+										}
+
+										edges.Remove(match);
+									}
+								}
+
+								foreach (var j in curedges)
+								{
+									edges.Add(j);
+								}
+							}
+
+							// unpaired edges
+							/*
+							foreach (var j in edges)
+							{
+								string line = string.Format("2 24 {0:F} {1:F} {2:F} {3:F} {4:F} {5:F}\n",
+									j.Item1.X, j.Item1.Y, j.Item1.Z,
+									j.Item2.X, j.Item2.Y, j.Item2.Z);
+								output.Write(Encoding.ASCII.GetBytes(line), 0, line.Length);
+							}
+							*/
+						}
+					}));
+					_progressBar.Value = 0;
 				}
 			}
+			_convertButton.Enabled = true;
 		}
 
 		private struct Vector3
@@ -79,16 +182,20 @@ namespace STLToLDraw
 				Z = z;
 			}
 
-			public double Length
+			public double Len
 			{
 				get { return Math.Sqrt(X * X + Y * Y + Z * Z); }
 			}
 
-			public Vector3 Norm
+			public double LenSq
 			{
-				get { double len = Length; return new Vector3(X / len, Y / len, Z / len); }
+				get { return X * X + Y * Y + Z * Z; }
 			}
 
+			public Vector3 Norm
+			{
+				get { double len = Len; return new Vector3(X / len, Y / len, Z / len); }
+			}
 
 			public double Dot(Vector3 other)
 			{
@@ -109,6 +216,16 @@ namespace STLToLDraw
 			{
 				return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
 			}
+
+			public static Vector3 operator *(Vector3 a, double b)
+			{
+				return new Vector3(a.X * b, a.Y * b, a.Z * b);
+			}
+
+			public static Vector3 operator /(Vector3 a, double b)
+			{
+				return new Vector3(a.X / b, a.Y / b, a.Z / b);
+			}
 		}
 
 		private struct Triangle
@@ -116,17 +233,42 @@ namespace STLToLDraw
 			public Vector3 A;
 			public Vector3 B;
 			public Vector3 C;
+			public Vector3 Normal;
 
-			public Triangle(Vector3 a, Vector3 b, Vector3 c)
+			public Triangle(Vector3 a, Vector3 b, Vector3 c, Vector3 normal)
 			{
-				A = a;
-				B = b;
-				C = c;
+				Normal = normal.Norm;
+
+				if ((b - a).Cross(c - a).Norm.Dot(Normal) < 0)
+				{
+					// wind in opposite direction
+					A = a;
+					B = c;
+					C = b;
+				}
+				else
+				{
+					A = a;
+					B = b;
+					C = c;
+				}
 			}
 
-			public Vector3 Normal
+			public List<Tuple<Vector3, Vector3, Triangle>> Edges
 			{
-				get { return (B - A).Cross(C - A).Norm; }
+				get
+				{
+					List<Tuple<Vector3, Vector3, Triangle>> r = new List<Tuple<Vector3, Vector3, Triangle>>();
+					r.Add(Tuple.Create(A, B, this));
+					r.Add(Tuple.Create(A, C, this));
+					r.Add(Tuple.Create(B, C, this));
+					return r;
+				}
+			}
+
+			public Vector3 Centre
+			{
+				get { return (A + B + C) / 3; }
 			}
 		}
 	}
